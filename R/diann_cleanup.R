bb461707d4b419eb9ec220254b94c98b90281ef8
caberryhill <causherm@iu.edu>
2025-11-03 11:11:45 -0700

    Cleaning up 11.3.25

--- R/diann_cleanup.R
+++ R/diann_cleanup.R
@@ -4,22 +4,24 @@
 #' @param df Unique protein groups matrix from DIANN
 #' @param kinases Either the human or mouse kinome spreadsheet
 #' @param peptides report.parquet from DIANN
+#' @param metadata File with column titled "Sample.ID" with the column names of the abundance values in the protein groups file, and a column titled "Treatment" with the corresponding treatment replicate
 #' @keywords DIANN
-#' @export
 #' @examples
 #' diann.cleanup()
+#' @import dplyr fuzzyjoin stringr tibble arsenal tidyverse tidyr data.table sjmisc ggpubr ggsci ggplot2 svglite rstatix pheatmap arrow
+#' @export
 
-diann.cleanup<- function(df, kinases ,peptides){
-  
+diann.cleanup<- function(df, kinases, metadata){
+
   #Matching to either the human or mouse kinome
   df.3<- kinases %>% inner_join(df, by=c("Gene"= "Genes"))
   df.3<- df.3[,-c(2)]
   print(("Matched to kinome"))
-  
+
   #filtering out <2 Razor + unique peptides and rows where the gene name is blank
-  
+
   dia.kinase.names<- as.vector(df.3$Gene)
-  
+
   peptides.kinases<- peptide[peptide$Genes %in% dia.kinase.names,]
   ## Get rid of duplicate precursor IDs and getting rid of peptides that aren't proteotypic
   peptides.kinases.2<- peptides.kinases[!duplicated(peptides.kinases$Precursor.Id),]
@@ -31,7 +33,7 @@ diann.cleanup<- function(df, kinases ,peptides){
   print(paste0("Number of kinases with only 1 peptide: ", y))
   df.3<- df.3[!df.3$Gene %in% kinase.1.peptide$Var1,]
   #Get rid of those kinases
-  
+
   #Log2 transform
   x<- df.3[,-c(1)]
   x<- log2(x)
@@ -40,12 +42,12 @@ diann.cleanup<- function(df, kinases ,peptides){
   df.3<- cbind(df.3, x)
   colnames(df.3)[1]<- "Genes"
   print("Log transformed")
-  #Match Sample ID to treatment 
-  df.4<-df.3 %>% 
-    rename_with(~deframe(sample)[.x], .cols = sample$Sample.ID) %>% 
-    dplyr::select(Genes, any_of(sample$Treatment))
+  #Match Sample ID to treatment
+  df.4<-df.3 %>%
+    rename_with(~deframe(metadata)[.x], .cols = metadata$Sample.ID) %>%
+    dplyr::select(Genes, any_of(metadata$Treatment))
   print("Matched to sample ID")
-  
+
   df.4<- as.data.frame(t(df.4))
   colnames(df.4)<- df.4[1,]
   df.4<- df.4[-c(1),]
@@ -53,12 +55,12 @@ diann.cleanup<- function(df, kinases ,peptides){
   df.4<- df.4[,c(ncol(df.4), 1:ncol(df.4)-1)]
   df.4$type<- sub("-.*", '', df.4$type)
   df.4$type<- sub("_.*", '', df.4$type)
-  
+
   x<- as.vector(NULL)
   g<- as.data.frame(table(df.4$type))
   g<-max(g$Freq)
-  
-  
+
+
   if(g>2){#Filter for columns where at least one group has 3 values
     for(i in 2:ncol(df.4)){
       a<- df.4[(1)]
@@ -69,7 +71,7 @@ diann.cleanup<- function(df, kinases ,peptides){
       df.6<- df.5 %>%
         group_by(type)%>%
         summarise(total_non_na = sum(!is.na(Kinase)))
-      
+
       if(any(df.6$total_non_na >2)){
         x<- append(x, y)
       }
@@ -84,16 +86,16 @@ diann.cleanup<- function(df, kinases ,peptides){
       df.6<- df.5 %>%
         group_by(type)%>%
         summarise(total_non_na = sum(!is.na(Kinase)))
-      
+
       if(any(df.6$total_non_na >1)){
         x<- append(x, y)
-        
+
       }
-      
-      
-      
+
+
+
     }
-  } else{ 
+  } else{
     for(i in 2:ncol(df.4)){
       a<- df.4[(1)]
       b<- df.4[i]
@@ -103,31 +105,31 @@ diann.cleanup<- function(df, kinases ,peptides){
       df.6<- df.5 %>%
         group_by(type)%>%
         summarise(total_non_na = sum(!is.na(Kinase)))
-      
+
       if(any(df.6$total_non_na==1)){
         x<- append(x, y)
-        
+
       }
-      
-      
-      
+
+
+
     }}
-  
-  
+
+
   print("Filtered for Kinases with enouch sample representation")
-  
+
   df.4<- df.4[colnames(df.4)%in% x]
   df.5<-data.frame(sapply(df.4, function(x) as.numeric(as.character(x))))
   rownames(df.5)<- rownames(df.4)
-  
-  #Impute 
+
+  #Impute
   impute_normal <- function(object, width=0.3, downshift=1.8, seed=100) {
-    
+
     if (!is.matrix(object)) object <- as.matrix(object)
     mx <- max(object, na.rm=TRUE)
     mn <- min(object, na.rm=TRUE)
     if (mx - mn > 20) warning("Please make sure the values are log-transformed")
-    
+
     set.seed(seed)
     object <- apply(object, 2, function(temp) {
       temp[!is.finite(temp)] <- NA
@@ -141,57 +143,57 @@ diann.cleanup<- function(df, kinases ,peptides){
     })
     return(object)
   }
-  
-  
+
+
   imputed_df<- impute_normal(df.5)
   imputed_df.2<- as.data.frame(imputed_df)
-  
+
   imputed_df.2$Treatment<- rownames(imputed_df.2)
   imputed_df.2$Treatment<- sub("-.*", '', imputed_df.2$Treatment)
   imputed_df.2$Treatment<- sub("_.*", '', imputed_df.2$Treatment)
   imputed_df.2<- imputed_df.2[,c(ncol(imputed_df.2), 1:ncol(imputed_df.2)-1)]
-  
+
   print("Imputed")
-  
+
   write.table(imputed_df.2, "Relative Kinase Protein Abundance.txt")
   write.csv(imputed_df.2, "Relative Kinase Protein Abundance.csv")
-  
+
   DF<- imputed_df.2
   kinases<-colnames(DF)
   #choose which columns to drop, typically this is the first column
   kinases<-kinases[-c(1)]
   #collapse to all kinases separated by + sign
   kinases<-paste(kinases,collapse="+")
-  
+
   ##Comparative Stats - All Groups ANOVA
   df<-summary(tableby(as.formula(paste('Treatment~',kinases)),
-                      numeric.stats=c("mean"), 
-                      stats.labels=list(mean="Mean"), 
-                      numeric.simplify=TRUE, 
+                      numeric.stats=c("mean"),
+                      stats.labels=list(mean="Mean"),
+                      numeric.simplify=TRUE,
                       cat.simplify=TRUE,
                       numeric.test="anova",
-                      total=FALSE, 
-                      digits.p=20, 
+                      total=FALSE,
+                      digits.p=20,
                       data=DF), text=NULL)
   df<-as.data.frame(df)
   write.csv(df, file="ANOVA.csv")
-  
+
   print("Completed Summary statistics")
   #PCA plots
-  
+
   new_df<- imputed_df.2
   new_df<- new_df[,c(-1)]
   cols.num<- colnames(new_df)
   new_df[cols.num]<- sapply(new_df[cols.num], as.numeric)
   df_pca<- prcomp(new_df)
-  
-  
+
+
   bc.pca.var<- df_pca$sdev^2
   bc.pca.var<- round(bc.pca.var/sum(bc.pca.var)*100,1)
   df.pca_data<- data.frame(sample= rownames(df_pca$x),
                            x=df_pca$x[,1],
                            y = df_pca$x[,2])
-  
+
   df.pca_data$type<- sub("_.*", "", df.pca_data$sample)
   df.pca_data$type<- sub("^ZZ.","", df.pca_data$type)
   df.pca_data$type<- sub("^Z.", "", df.pca_data$type)
@@ -199,10 +201,10 @@ diann.cleanup<- function(df, kinases ,peptides){
     group_by(type) %>%
     mutate(Rep = row_number()) %>%
     ungroup()
-  
+
   df.pca_data$Rep<- as.character(as.numeric(df.pca_data$Rep))
-  
-  a<-ggplot(data=df.pca_data,aes(label=sample,x=x,y=y, color = type, shape = Rep)) + 
+
+  a<-ggplot(data=df.pca_data,aes(label=sample,x=x,y=y, color = type, shape = Rep)) +
     ggtitle("Kinome PCA Plot") +
     geom_point(aes( size = 4)) +
     xlab(paste("PC1: ",bc.pca.var[1],"%",sep="")) +
@@ -212,42 +214,42 @@ diann.cleanup<- function(df, kinases ,peptides){
     theme(legend.text = element_text(size = 10))+
     guides(color=guide_legend(override.aes = list(size = 5)),
            shape = guide_legend(override.aes = list(size=5)))
-  
-  
+
+
   ggsave("PCA plot.svg", a, device= "svg", width = 10, height = 6)
   ggsave("PCA plot.pdf", a, device= "pdf", width = 10, height = 6)
-  
+
   print("Made PCA plot")
   #Generating a heatmap and clustering using Euclidian distance
-  
+
   new_df$type<- rownames(new_df)
   new_df$type<- sub("_.*", "", new_df$type)
   new_df$type<- sub("^ZZ.","", new_df$type)
   new_df$type<- sub("^Z.", "", new_df$type)
-  
+
   new_df<-new_df %>%
     group_by(type) %>%
     mutate(Rep = row_number()) %>%
     ungroup()
-  
+
   new_df$Rep<- as.character(as.numeric(new_df$Rep))
   new_df$type<- paste0(new_df$type,"-Rep",new_df$Rep)
   new_df2<- new_df[,-((ncol(new_df)-1):ncol(new_df))]
   rownames(new_df2)<- new_df$type
   ## Z-score of the replicates
-  
+
   df.matrix<- as.matrix(new_df2)
   df.matrix<- as.data.frame(t(df.matrix))
-  
+
   cal_z_score <- function(f){
     (f - mean(f)) / sd(f)
   }
-  
+
   df.matrix.z.score <- t(apply(df.matrix, 1, cal_z_score))
-  
-  
+
+
   svg("Heatmap of the z-score of the Log2 LFQ kinome intensities by replicate.svg", width = 10, height = 10)
-  
+
   pheatmap(df.matrix.z.score,
            cluster_rows = T,
            cluster_cols = T,
@@ -258,7 +260,7 @@ diann.cleanup<- function(df, kinases ,peptides){
            colorRampPalette(c("#000080", "white", "#DC143C"))(100),
            angle_col = 45,
            main="Z-score of the Log2 of the Kinase LFQ intensity (By Replicate)")
-  
+
   dev.off()
   print("Made heatmap of each replicate")
   ## Z-score of the average
@@ -272,9 +274,9 @@ diann.cleanup<- function(df, kinases ,peptides){
   df.matrix<- as.matrix(new_df4)
   df.matrix<- as.data.frame(t(new_df4))
   df.matrix.z.score2 <- t(apply(df.matrix, 1, cal_z_score))
-  
+
   svg("Heatmap of the z-score of the averaged Log2 LFQ kinome intensities.svg", width = 10, height = 10)
-  
+
   pheatmap(df.matrix.z.score2,
            cluster_rows = T,
            cluster_cols = T,
@@ -285,10 +287,10 @@ diann.cleanup<- function(df, kinases ,peptides){
            colorRampPalette(c("#000080", "white", "#DC143C"))(100),
            angle_col = 45,
            main="Z-score of the Log2 of the Averaged Kinase LFQ intensity")
-  
+
   dev.off()
   print("Made heatmap of averaged replicates")
-  
+
   # Heatmap of average of significant kinases
   colnames(df)[1]<- "Kinases"
   sig.output<- df %>% filter(`p value` <= 0.05)
@@ -298,9 +300,9 @@ diann.cleanup<- function(df, kinases ,peptides){
   df.matrix.z.score2$Kinases<- rownames(df.matrix.z.score2)
   df.matrix.z.score2<- df.matrix.z.score2[df.matrix.z.score2$Kinases %in% sig.output$Kinases,]
   df.matrix.z.score2<- df.matrix.z.score2[,-c(ncol(df.matrix.z.score2))]
-  
+
   svg("Heatmap of the z-score of the averaged Log2 LFQ significant kinases.svg", width = 10, height = 10)
-  
+
   pheatmap(df.matrix.z.score2,
            cluster_rows = T,
            cluster_cols = T,
@@ -311,10 +313,10 @@ diann.cleanup<- function(df, kinases ,peptides){
            colorRampPalette(c("#000080", "white", "#DC143C"))(100),
            angle_col = 45,
            main="Z-score of the Log2(Kinase LFQ intensity)")
-  
+
   dev.off()
   print("Made heatmap of averaged replicates - signficant")
-  
-  
+
+
   return(imputed_df.2)
 }
--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # MIB-MS
 MIB-MS kinome analysis package and protocols
+
+This package uses either MaxQuant LFQ or DIA-NN protein level data and filters for: 1) kinases (either human or mouse),2) proteins with >1 unique peptide, and 3) kinases quantified in all replicates of at least one sample.
+
+Users must also create a metadata file to match the protein column names with the correct treatment replicate. An example can be found in the data folder.
+
+To force the correct comparison, users may put "Z." in front of the control sample treatment name, thus creating the Treatment vs. Control comparison.
+
+For DIA-NN, the report.parquet file is also required.
+
+Below is how to set up the env and run the script:
+
+
+```r
+library(mibms)
+library(arrow)
+
+data("mouse.kinome") #Load pre-installed kinome data
+
+
+df<-read.csv("proteinGroups.csv", check.names = F)
+metadata<-read.csv("Sample.csv")
+peptide<- read_parquet("report.parquet")
+kinome.analysis<- diann.cleanup(df = df, sample = metadata, mouse.kinome)
+stats<- statistical.testing(DF = kinome.analysis)
+```
