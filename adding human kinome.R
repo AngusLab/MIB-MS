2dc93cd51b3476c9f3a525c592834ed9a911cf4c
caberryhill <causherm@iu.edu>
2026-02-24 14:50:04 -0700

    Updated script and Lab Protocols

--- adding human kinome.R
+++ adding human kinome.R
@@ -1,9 +1,13 @@
-#Adding exported data (human.kinome and mouse.kinome)
+#Adding exported data (human.kinome and mouse.kinome and proteome)
 
 human.kinome<- read.csv("human.kinome.csv")
 mouse.kinome<- read.csv("mouse.kinome.csv")
+human.proteome<-read.csv("Proteome.names.csv")
+mouse.proteome<- read.csv("Mouse_Proteome.names.csv")
 
 usethis::use_data(human.kinome)
 usethis::use_data(mouse.kinome)
+usethis::use_data(human.proteome)
+usethis::use_data(mouse.proteome)
 
 
